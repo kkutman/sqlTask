29a37ada856c317c3d2b9310acc8eb0b47742b0d
КМ <kkutman18@gmail.com>
2023-01-10 19:28:27 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,80 @@
+/**
+ * name : kutman
+ **/
+public class Main {
+    public static void main(String[] args) {
+        /*
+1. Кардардын атын, шаарын чыгарыныз
+2. Кардарлардын олкосун (уникалдуу) чыгарыныз
+3. Кардарлардын олкосунун (уникалдуу) санын чыгарыш керек
+4.Мексикалык кардарларды чыгарыныз
+5. Айдиси#1 болгон кардардын шаарын, дарегин чыгарыныз
+6. Германиялык же Испаниялык кардарларды чыгарыныз
+7. Кардарлардын олкосун алфавит менен, атын алфавиттин аягы менен сорттоп чыгарыныз
+8. Дареги жазылбаган кардарларды чыгарыныз
+9. Продуктулардын минимальный баасын чыгарыныз
+10. Канча продукт бар экенин чыгарыныз
+11. Баш тамгасы ‘А’ башталган бардык кардарлардын атын чыгарыныз
+12. Германиялык, Франциялык, Британиялык кардарларды чыгарыныз
+13. Поставщик менен бир олкодон болгон кардарларды чыгарыныз
+14. 1996-жылкы жалпы заказдарды чыгарыныз
+         */
+
+        /*
+        #1 Display only car’s brands & car’s models & car’s price
+#1 car tablicanyn ichinen tolko mashinanyn brendin jana modelin jana baasyn chygarynyz
+#1 Отображать только марки автомобилей и модели автомобилей и цену автомобиля
+#2 Display quantity of all cars
+#2 car tablicasynyn ichinen baardyk mashinalardyn sanyn chygarynyz
+#2 Показать количество всех автомобилей
+#3 Display all cars where brand is Hyundai
+#3 Brendi Hyundaiga barabar bolgon baardyk mashinalardy chygarynyz
+#3 Показать все автомобили марки Hyundai
+#4 Display all Red and Blue cars
+#4 Baardyk Kyzyl jana Kok bolgon mashinalardy chygarynyz
+#4 Показать все красные и синие автомобили
+#5 Display all cars where issued year between 2000 and 2010
+#5 Baardyk 2000 menen 2010chu jyldary arasynda chygarylgan mashinalardy chygarynyz
+#5 Показать все автомобили, выпущенные с 2000 по 2010 год
+#6 Display quantity of cars where car’s brand is Chevrolet
+#6 Chevrolet brendinde kancha machine bar ekendigin chygarynyz
+#6 Показать количество автомобилей марки Chevrolet
+#7 Display the average year of manufacture of cars
+#7 Mashinalardyn jyldarynyn ortocho maanisin* (srednoe znachenie) tabynyz
+#7 Отображение среднего года выпуска автомобилей
+#8 Display the cars where brand equals to Audi, Toyota, Kia and Ford
+#8 Audi, Toyota, Kia jana Ford, ushul markadagy baardyk mashinalardy chygargyla
+# 8 Вывести все автомобили брендов Audi, Toyota, Kia и Ford
+#9 Display cars where car’s model starts with ‘T’
+#9 Mashinanyn modeli ‘T’ menen bashtalgan mashinalardy chygarynyz
+# 9 Показать автомобили, где модель автомобиля начинается с «Т»
+#10 Display cars where car’s model end with ‘e’;
+#10 Mashinanyn modeli ‘e’ menen button mashinalardy chygarynyz
+#10 Показать автомобили, где модель автомобиля заканчивается на «е»;
+#11 Display brands where contains only 5 symbols
+#11 5 symbol kamtygan brandderdi consolgo chygarynyz
+#11 Показать бренды, содержащие только 5 символов
+#12 Display all amounts where car’s brand is Mercedes-Benz
+#12 brandi Mercedes-Benz bolgon mashinalardyn obshiy baasyn chygarynyz
+#12 Показать общую сумму всех автомобилей марки Mercedes-Benz
+#13 Display the most expensive car & the cheepest car
+#13 en kymbat jana en arzan mashinany chygarynyz
+#13 Показать самую дорогую и самую дешевую машину
+#14 Display all cars where car’s brand not TOYOTA
+#14 TOYOTAdan bashkasynyn baaryn chygarynyz
+#14 Показать все автомобили, где марка автомобиля не TOYOTA
+#15 Display 10 the most expensive cars
+#15 En kymbat 10 mahsinany chygarynyz
+#15 Показать 10 самых дорогих автомобилей
+#16 Display the newest cars between 5th to 15th
+#16 En jany mashinalardyn 5ten bashtap 15chige cheinki mashinalardy chygarynyz
+#16 Отображение новейших автомобилей с 5-го по 15-е.
+#17 Display cars where car’s year of issue not between 1995 and 2005;
+#17 1995 jana 2005 jyldar arasyndagy mashinalardan bashka baardyk mashinalardy chygarynyz
+#17 Вывести все автомобили, кроме тех автомобилей которые выпустились между 1995 и 2005;
+#18 Display most cars in one color
+#18 en kop mashinasy bar tustordu korsotunuzdor i kancha mashinesi bar ekendigin dagy korsotunuzdor
+# 18 Вывести автомобилей одного цвета и наибольшего количества
+         */
+    }
+}
